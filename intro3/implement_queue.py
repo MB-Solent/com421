7dfd5dfce5b360c40cd77962405b0f497bfd7b95
mihai <4badem14@solent.ac.uk>
2022-10-18 13:53:14 +0100

    queue fully implemented

--- intro3/implement_queue.py
+++ intro3/implement_queue.py
@@ -1,51 +1,69 @@
 class Queue:
     def __init__(self, capacity=5):
-        self.internal_array = [None] * capacity
 
+        self.internal_array = [None] * capacity
         self.queue_start = 0
         self.queue_end = 0
-        self.size = 0
+        self.queue_size = 0
 
-    def add(self, item):
+    def add(self, *args):
 
-        if self.size == len(self.internal_array):
-            print("Queue is full, cannot add more.")
+        for item in args:
 
-        else:
-            self.internal_array[self.queue_end] = item
+            # check if queue has space
+            if self.queue_size < len(self.internal_array):
 
-            self.size += 1
+                # if not full, add new item at end of queue
 
-            self.queue_end += 1
+                self.internal_array[self.queue_end] = item
+                print(f"Added {item} to position {self.queue_end}")
 
-            if self.queue_end >= len(self.internal_array):
-                self.queue_end = 0
+                # increase queue size variable
+
+                self.queue_size += 1
+
+                # update end of queue
+
+                self.queue_end += 1
+
+                # wrap around queue end position
+
+                if self.queue_end >= len(self.internal_array):
+
+                    self.queue_end = 0
+
+                print(f"Queue end position changed to {self.queue_end}")
+
+            else:
+
+                print("Not enough space to add another item.")
+                break
 
     def remove(self, amount):
 
-        # index to start looping through the internal array from
-        if self.size != 0:
+        # check if already empty
 
-            # Limit amount to remove to a max of the current size
-            if amount > self.size:
-                amount = self.size
+        for _ in range(amount):
 
-            # Set the starting point to loop through the queue
-            index = self.queue_start
+            if self.queue_size > 0:
 
-            for count in range(amount):
+                print(f"Removed {self.internal_array[self.queue_start]} from position the start position {self.queue_start}")
 
-                # remove the item from the queue and decrease size
-                self.internal_array[index] = None
-                self.size -= 1
+                self.internal_array[self.queue_start] = None
 
-                # go to next index, if out of the array size, go back to start.
-                index += 1
-                if index >= len(self.internal_array):
-                    index = 0
+                self.queue_size -= 1
 
-        else:
-            print("Can't remove from queue, it's currently empty.")
+                self.queue_start += 1
+
+                if self.queue_start >= len(self.internal_array):
+
+                    self.queue_start = 0
+
+                print(f"The start of the queue is now at position {self.queue_start}")
+
+            else:
+                print("The queue is empty.")
+                break
 
     def __str__(self):
         return self.internal_array.__str__()
@@ -55,10 +73,27 @@ printer_queue = Queue(6)
 
 print(printer_queue)
 
-print(printer_queue.size)
+print(len(printer_queue.internal_array))
+print(printer_queue.queue_size)
 
-printer_queue.add("jpg")
-
-print(printer_queue.size)
+printer_queue.add("a")
+printer_queue.add("b", "c")
 
 print(printer_queue)
+
+printer_queue.remove(2)
+
+print(printer_queue)
+
+printer_queue.add("d", "e", "f", "g", "h")
+print(printer_queue)
+
+printer_queue.add("i")
+
+printer_queue.remove(2)
+
+print(printer_queue)
+printer_queue.add("i")
+printer_queue.remove(1)
+
+
