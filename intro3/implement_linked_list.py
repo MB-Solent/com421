2036cf860e4eedbc8848ea5532ba4119112f0a5f
mihai <4badem14@solent.ac.uk>
2022-10-18 14:52:48 +0100

    started implementing linked list

---
+++ intro3/implement_linked_list.py
@@ -0,0 +1,15 @@
+class Node:
+
+    def __init__(self, data):
+
+        self.data = data
+        self.prev = None
+        self.next = None
+
+    def link(self, new_next):
+        pass
+
+class LinkedList:
+
+    def __init__(self):
+        pass
