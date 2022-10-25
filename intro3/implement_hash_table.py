67b4eb94d24cf01bee6887e8a2ab1bfb409f639e
mihai <4badem14@solent.ac.uk>
2022-10-25 15:41:51 +0100

    created implement_hash_table.py and wrote code for managing a hash table using a basic hash functions

---
+++ intro3/implement_hash_table.py
@@ -0,0 +1,37 @@
+class HashTable:
+
+    def __init__(self, capacity):
+        self.capacity = capacity
+        bucket = []
+        self.bucket_list = [[] for _ in range(self.capacity)]
+
+    def get(self, key):
+        bucket_id = self.calc_hash_code(key) % self.capacity
+        for item in self.bucket_list[bucket_id]:
+            if item[0] == key:
+                return item[1]  # return valueS
+        pass
+
+    def put(self, key, value):
+        bucket_id = self.calc_hash_code(key) % self.capacity
+        self.bucket_list[bucket_id].append((key, value))
+
+    def calc_hash_code(self, key):
+        hash_code = 0
+        for character in key:
+            hash_code += ord(character)
+        return hash_code
+
+    def __str__(self):
+        return self.bucket_list.__str__()
+
+
+x = HashTable(10)
+
+print(x)
+print(x.calc_hash_code("test") % 10)
+x.put("test", 5)
+x.put()
+print(x)
+
+print(x.get("test"))
