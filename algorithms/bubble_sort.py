3c538507c542611fe8aa9d2d61df96eb97d2b2e7
mihai <4badem14@solent.ac.uk>
2022-11-08 14:25:17 +0000

    bubble_sort.py file created, implemented bubble sort algorithm in python

---
+++ algorithms/bubble_sort.py
@@ -0,0 +1,30 @@
+import random
+
+
+def bubble_sort(list_reference):
+    for i in range(len(list_reference) - 1):
+        for j in range(len(list_reference) - 1 - i - 1):
+            if list_reference[j] > list_reference[j+1]:
+                temp = list_reference[j]
+                list_reference[j] = list_reference[j + 1]
+                list_reference[j + 1] = temp
+
+
+def run():
+    numbers = random.sample(range(1, 20), 5)
+
+    print(numbers)
+
+    input()
+
+    bubble_sort(numbers)
+
+    print(numbers)
+
+
+if __name__ == "__main__":
+    run()
+
+
+
+
