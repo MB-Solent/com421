77586fb19d75b0efc269e586d73c2d6bf441a7b9
mihai <4badem14@solent.ac.uk>
2022-11-10 11:10:29 +0000

    insertion_sort.py file created, code partially written, not completed yet

--- algorithms/insertion_sort.py
+++ algorithms/insertion_sort.py
@@ -2,4 +2,17 @@ import unsorted_data
 
 
 def insertion_sort(list_reference):
-    pass
+    for i in range(1,len(list_reference)):
+        pass
+
+
+def run():
+    numbers = unsorted_data.random_list()
+    # print(numbers)
+    insertion_sort(numbers)
+    # print(numbers)
+
+
+if __name__ == "__main__":
+    run()
+
