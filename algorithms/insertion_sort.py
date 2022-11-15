8eab30a532ec3ee9c8788652aa4d6f2394f48bf6
mihai <4badem14@solent.ac.uk>
2022-11-15 13:34:07 +0000

    insertion sort implemented and tested

--- algorithms/insertion_sort.py
+++ algorithms/insertion_sort.py
@@ -2,15 +2,20 @@ import unsorted_data
 
 
 def insertion_sort(list_reference):
-    for i in range(1,len(list_reference)):
-        pass
+    for i in range(1, len(list_reference)):
+        divider_value = list_reference[i]
+        j = i - 1
+        while list_reference[j] > divider_value and j >= 0:
+            list_reference[j+1] = list_reference[j]
+            j -= 1
+        list_reference[j+1] = divider_value
 
 
 def run():
-    numbers = unsorted_data.random_list()
-    # print(numbers)
+    numbers = unsorted_data.random_list(100, 50)
+    print(numbers)
     insertion_sort(numbers)
-    # print(numbers)
+    print(numbers)
 
 
 if __name__ == "__main__":
